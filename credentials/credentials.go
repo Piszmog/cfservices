1810762396ed79d6b84b6d8d5f4f2ddee8662e94
Piszmog <piszmogcloud@gmail.com>
2018-07-30 19:21:24 -0600

    format code

--- credentials/credentials.go
+++ credentials/credentials.go
@@ -1,14 +1,14 @@
 package credentials
 
 type Credentials struct {
-    Uri            string `json:"uri"`
-    APIUri         string `json:"http_api_uri"`
-    LicenceKey     string `json:"licenseKey"`
-    ClientSecret   string `json:"client_secret"`
-    ClientId       string `json:"client_id"`
-    AccessTokenUri string `json:"access_token_uri"`
-    Hostname       string `json:"hostname"`
-    Username       string `json:"username"`
-    Password       string `json:"password"`
-    Port           string `json:"port"`
+	Uri            string `json:"uri"`
+	APIUri         string `json:"http_api_uri"`
+	LicenceKey     string `json:"licenseKey"`
+	ClientSecret   string `json:"client_secret"`
+	ClientId       string `json:"client_id"`
+	AccessTokenUri string `json:"access_token_uri"`
+	Hostname       string `json:"hostname"`
+	Username       string `json:"username"`
+	Password       string `json:"password"`
+	Port           string `json:"port"`
 }
